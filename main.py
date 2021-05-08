1fab781ea225530dffcac9d00b2ed071fb492227
Daniel L. Lu <daniel@lawrence.lu>
2021-05-07 20:01:40 -0700

    use my own matcher

--- main.py
+++ main.py
@@ -3,16 +3,40 @@
 import cv2
 import numpy as np
 import superpoint as sp
+import scipy.optimize
 
 from ouster import client, pcap
 from contextlib import closing
 from more_itertools import nth
 
+
+def match(desc1, desc2, thresh=0.6):
+    dmat = np.dot(desc1.T, desc2)
+    dmat = np.sqrt(2 - 2 * np.clip(dmat, -1, 1))
+    matches = scipy.optimize.linear_sum_assignment(dmat, maximize=False)
+    matches = [z for z in zip(*matches) if dmat[z[0], z[1]] < thresh]
+    return matches
+
+
+def plotmatch(img1, img2, pts1, pts2, matches):
+    img = np.concatenate((img1, img2), axis=0)
+    for m1, m2 in matches:
+        p1 = (int(pts1[0, m1]), int(pts1[1, m1]))
+        p2 = (int(pts2[0, m2]), int(pts2[1, m2] + 128))
+        cv2.line(img, p1, p2, [0, 100, 255], thickness=2)
+    return img
+
+def low_latency_odometry(xyz1, xyz2, pts1, pts2, matches):
+    for m1, m2 in matches:
+        p1 = xyz1[int(pts1[0, m1]), int(pts1[1, m1])]
+        p2 = xyz2[int(pts2[0, m2]), int(pts2[1, m2])]
+
+
 pcap_path = 'OS2_128.pcap'
 metadata_path = 'OS2_1024x20_128.json'
 
-with open(metadata_path, 'r') as f:
-    metadata = client.SensorInfo(f.read())
+metadata = client.SensorInfo(open(metadata_path).read())
+xyzlut = client.XYZLut(metadata)
 
 source = pcap.Pcap(pcap_path, metadata)
 
@@ -22,28 +46,59 @@ fe = sp.SuperPointFrontend(weights_path='superpoint_v1.pth',
                            nn_thresh=0.6,
                            cuda=True)
 print('==> Successfully loaded pre-trained network.')
-point_tracker = sp.PointTracker(max_length=5, nn_thresh=fe.nn_thresh)
+frame = 0
 
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+zoo_desc = None
+zoo_pts = None
+
+orb = cv2.ORB_create()
+
+prev_img = None
 with closing(client.Scans(source)) as stream:
     for scan in stream:
+        if frame == 0:
+            frame += 1
+            continue
         range_field = scan.field(client.ChanField.RANGE)
         range_img = client.destagger(source.metadata, range_field)
         intensity_field = scan.field(client.ChanField.SIGNAL)
         intensity_img = client.destagger(source.metadata, intensity_field)
+        xyz_destaggered = client.destagger(metadata, xyzlut(scan))
+        print('xyz', xyz_destaggered.shape)
 
         signal = client.destagger(stream.metadata,
                                   scan.field(client.ChanField.SIGNAL))
         signal = np.sqrt(signal)
         signal = signal / np.max(signal)
         signal = signal.astype(np.float32)
-
-        pts, desc, heatmap = fe.run(signal)
-        point_tracker.update(pts, desc)
-        point_tracks = point_tracker.get_tracks(2)
-
         img = (signal * 255).astype(np.uint8)
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        point_tracker.draw_tracks(img, point_tracks)
 
-        cv2.imshow("img", img)
+        pts, desc, heatmap = fe.run(signal)
+        print('sp', pts.shape, desc.shape, type(desc.dtype))
+
+        #pts, desc = orb.detectAndCompute(img, None)
+        #pts = np.array([[k.pt[0], k.pt[1]] for k in pts])
+        #pts = pts.T
+        #print('orb', pts.shape, desc.shape, type(desc.dtype))
+        if frame == 1:
+            zoo_desc = desc
+            zoo_pts = pts
+
+            frame += 1
+            prev_img = img
+            continue
+
+        matches = match(desc, zoo_desc)
+        mimg = plotmatch(img, prev_img, pts, zoo_pts, matches)
+        cv2.imwrite('output_{:06}.png'.format(frame), mimg)
+
+        zoo_desc = desc
+        zoo_pts = pts
+        frame += 1
+        prev_img = img
+
+        cv2.imshow("img", mimg)
         key = cv2.waitKey(1) & 0xFF
--- superpoint.py
+++ superpoint.py
@@ -1,15 +1,6 @@
-import cv2
-import glob
 import numpy as np
 import os
 import torch
-
-# Jet colormap for visualization.
-myjet = np.array([[0., 0., 0.5], [0., 0., 0.99910873], [0., 0.37843137, 1.],
-                  [0., 0.83333333, 1.], [0.30044276, 1., 0.66729918],
-                  [0.66729918, 1., 0.30044276], [1., 0.90123457, 0.],
-                  [1., 0.48002905, 0.], [0.99910873, 0.07334786, 0.],
-                  [0.5, 0., 0.]])
 
 
 class SuperPointNet(torch.nn.Module):
@@ -85,13 +76,13 @@ class SuperPointNet(torch.nn.Module):
 
     def forward(self, x):
         """ Forward pass that jointly computes unprocessed point and descriptor
-    tensors.
-    Input
-      x: Image pytorch tensor shaped N x 1 x H x W.
-    Output
-      semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
-      desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
-    """
+        tensors.
+        Input
+          x: Image pytorch tensor shaped N x 1 x H x W.
+        Output
+          semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+          desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+        """
         # Shared Encoder.
         x = self.relu(self.conv1a(x))
         x = self.relu(self.conv1b(x))
@@ -146,30 +137,30 @@ class SuperPointFrontend(object):
 
     def nms_fast(self, in_corners, H, W, dist_thresh):
         """
-    Run a faster approximate Non-Max-Suppression on numpy corners shaped:
-      3xN [x_i,y_i,conf_i]^T
+        Run a faster approximate Non-Max-Suppression on numpy corners shaped:
+          3xN [x_i,y_i,conf_i]^T
 
-    Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
-    are zeros. Iterate through all the 1's and convert them either to -1 or 0.
-    Suppress points by setting nearby values to 0.
+        Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
+        are zeros. Iterate through all the 1's and convert them either to -1 or 0.
+        Suppress points by setting nearby values to 0.
 
-    Grid Value Legend:
-    -1 : Kept.
-     0 : Empty or suppressed.
-     1 : To be processed (converted to either kept or supressed).
+        Grid Value Legend:
+        -1 : Kept.
+         0 : Empty or suppressed.
+         1 : To be processed (converted to either kept or supressed).
 
-    NOTE: The NMS first rounds points to integers, so NMS distance might not
-    be exactly dist_thresh. It also assumes points are within image boundaries.
+        NOTE: The NMS first rounds points to integers, so NMS distance might not
+        be exactly dist_thresh. It also assumes points are within image boundaries.
 
-    Inputs
-      in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
-      H - Image height.
-      W - Image width.
-      dist_thresh - Distance to suppress, measured as an infinty norm distance.
-    Returns
-      nmsed_corners - 3xN numpy matrix with surviving corners.
-      nmsed_inds - N length numpy vector with surviving corner indices.
-    """
+        Inputs
+          in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+          H - Image height.
+          W - Image width.
+          dist_thresh - Distance to suppress, measured as an infinty norm distance.
+        Returns
+          nmsed_corners - 3xN numpy matrix with surviving corners.
+          nmsed_inds - N length numpy vector with surviving corner indices.
+        """
         grid = np.zeros((H, W)).astype(int)  # Track NMS data.
         inds = np.zeros((H, W)).astype(int)  # Store indices of points.
         # Sort by confidence and round to nearest int.
@@ -212,13 +203,13 @@ class SuperPointFrontend(object):
 
     def run(self, img):
         """ Process a numpy image to extract points and descriptors.
-    Input
-      img - HxW numpy float32 input image in range [0,1].
-    Output
-      corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
-      desc - 256xN numpy array of corresponding unit normalized descriptors.
-      heatmap - HxW numpy heatmap in range [0,1] of point confidences.
-      """
+        Input
+          img - HxW numpy float32 input image in range [0,1].
+        Output
+          corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+          desc - 256xN numpy array of corresponding unit normalized descriptors.
+          heatmap - HxW numpy heatmap in range [0,1] of point confidences.
+        """
         assert img.ndim == 2, 'Image must be grayscale.'
         assert img.dtype == np.float32, 'Image must be float32.'
         H, W = img.shape[0], img.shape[1]
@@ -281,304 +272,3 @@ class SuperPointFrontend(object):
             desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
         return pts, desc, heatmap
 
-
-class PointTracker(object):
-    """ Class to manage a fixed memory of points and descriptors that enables
-  sparse optical flow point tracking.
-
-  Internally, the tracker stores a 'tracks' matrix sized M x (2+L), of M
-  tracks with maximum length L, where each row corresponds to:
-  row_m = [track_id_m, avg_desc_score_m, point_id_0_m, ..., point_id_L-1_m].
-  """
-    def __init__(self, max_length, nn_thresh):
-        if max_length < 2:
-            raise ValueError('max_length must be greater than or equal to 2.')
-        self.maxl = max_length
-        self.nn_thresh = nn_thresh
-        self.all_pts = []
-        for n in range(self.maxl):
-            self.all_pts.append(np.zeros((2, 0)))
-        self.last_desc = None
-        self.tracks = np.zeros((0, self.maxl + 2))
-        self.track_count = 0
-        self.max_score = 9999
-
-    def nn_match_two_way(self, desc1, desc2, nn_thresh):
-        """
-    Performs two-way nearest neighbor matching of two sets of descriptors, such
-    that the NN match from descriptor A->B must equal the NN match from B->A.
-
-    Inputs:
-      desc1 - NxM numpy matrix of N corresponding M-dimensional descriptors.
-      desc2 - NxM numpy matrix of N corresponding M-dimensional descriptors.
-      nn_thresh - Optional descriptor distance below which is a good match.
-
-    Returns:
-      matches - 3xL numpy array, of L matches, where L <= N and each column i is
-                a match of two descriptors, d_i in image 1 and d_j' in image 2:
-                [d_i index, d_j' index, match_score]^T
-    """
-        assert desc1.shape[0] == desc2.shape[0]
-        if desc1.shape[1] == 0 or desc2.shape[1] == 0:
-            return np.zeros((3, 0))
-        if nn_thresh < 0.0:
-            raise ValueError('\'nn_thresh\' should be non-negative')
-        # Compute L2 distance. Easy since vectors are unit normalized.
-        dmat = np.dot(desc1.T, desc2)
-        dmat = np.sqrt(2 - 2 * np.clip(dmat, -1, 1))
-        # Get NN indices and scores.
-        idx = np.argmin(dmat, axis=1)
-        scores = dmat[np.arange(dmat.shape[0]), idx]
-        # Threshold the NN matches.
-        keep = scores < nn_thresh
-        # Check if nearest neighbor goes both directions and keep those.
-        idx2 = np.argmin(dmat, axis=0)
-        keep_bi = np.arange(len(idx)) == idx2[idx]
-        keep = np.logical_and(keep, keep_bi)
-        idx = idx[keep]
-        scores = scores[keep]
-        # Get the surviving point indices.
-        m_idx1 = np.arange(desc1.shape[1])[keep]
-        m_idx2 = idx
-        # Populate the final 3xN match data structure.
-        matches = np.zeros((3, int(keep.sum())))
-        matches[0, :] = m_idx1
-        matches[1, :] = m_idx2
-        matches[2, :] = scores
-        return matches
-
-    def get_offsets(self):
-        """ Iterate through list of points and accumulate an offset value. Used to
-    index the global point IDs into the list of points.
-
-    Returns
-      offsets - N length array with integer offset locations.
-    """
-        # Compute id offsets.
-        offsets = []
-        offsets.append(0)
-        for i in range(len(self.all_pts) -
-                       1):  # Skip last camera size, not needed.
-            offsets.append(self.all_pts[i].shape[1])
-        offsets = np.array(offsets)
-        offsets = np.cumsum(offsets)
-        return offsets
-
-    def update(self, pts, desc):
-        """ Add a new set of point and descriptor observations to the tracker.
-
-    Inputs
-      pts - 3xN numpy array of 2D point observations.
-      desc - DxN numpy array of corresponding D dimensional descriptors.
-    """
-        if pts is None or desc is None:
-            print('PointTracker: Warning, no points were added to tracker.')
-            return
-        assert pts.shape[1] == desc.shape[1]
-        # Initialize last_desc.
-        if self.last_desc is None:
-            self.last_desc = np.zeros((desc.shape[0], 0))
-        # Remove oldest points, store its size to update ids later.
-        remove_size = self.all_pts[0].shape[1]
-        self.all_pts.pop(0)
-        self.all_pts.append(pts)
-        # Remove oldest point in track.
-        self.tracks = np.delete(self.tracks, 2, axis=1)
-        # Update track offsets.
-        for i in range(2, self.tracks.shape[1]):
-            self.tracks[:, i] -= remove_size
-        self.tracks[:, 2:][self.tracks[:, 2:] < -1] = -1
-        offsets = self.get_offsets()
-        # Add a new -1 column.
-        self.tracks = np.hstack((self.tracks, -1 * np.ones(
-            (self.tracks.shape[0], 1))))
-        # Try to append to existing tracks.
-        matched = np.zeros((pts.shape[1])).astype(bool)
-        matches = self.nn_match_two_way(self.last_desc, desc, self.nn_thresh)
-        for match in matches.T:
-            # Add a new point to it's matched track.
-            id1 = int(match[0]) + offsets[-2]
-            id2 = int(match[1]) + offsets[-1]
-            found = np.argwhere(self.tracks[:, -2] == id1)
-            if found.shape[0] > 0:
-                matched[int(match[1])] = True
-                row = int(found)
-                self.tracks[row, -1] = id2
-                if self.tracks[row, 1] == self.max_score:
-                    # Initialize track score.
-                    self.tracks[row, 1] = match[2]
-                else:
-                    # Update track score with running average.
-                    # NOTE(dd): this running average can contain scores from old matches
-                    #           not contained in last max_length track points.
-                    track_len = (self.tracks[row, 2:] != -1).sum() - 1.
-                    frac = 1. / float(track_len)
-                    self.tracks[row, 1] = (
-                        1. - frac) * self.tracks[row, 1] + frac * match[2]
-        # Add unmatched tracks.
-        new_ids = np.arange(pts.shape[1]) + offsets[-1]
-        new_ids = new_ids[~matched]
-        new_tracks = -1 * np.ones((new_ids.shape[0], self.maxl + 2))
-        new_tracks[:, -1] = new_ids
-        new_num = new_ids.shape[0]
-        new_trackids = self.track_count + np.arange(new_num)
-        new_tracks[:, 0] = new_trackids
-        new_tracks[:, 1] = self.max_score * np.ones(new_ids.shape[0])
-        self.tracks = np.vstack((self.tracks, new_tracks))
-        self.track_count += new_num  # Update the track count.
-        # Remove empty tracks.
-        keep_rows = np.any(self.tracks[:, 2:] >= 0, axis=1)
-        self.tracks = self.tracks[keep_rows, :]
-        # Store the last descriptors.
-        self.last_desc = desc.copy()
-        return
-
-    def get_tracks(self, min_length):
-        """ Retrieve point tracks of a given minimum length.
-    Input
-      min_length - integer >= 1 with minimum track length
-    Output
-      returned_tracks - M x (2+L) sized matrix storing track indices, where
-        M is the number of tracks and L is the maximum track length.
-    """
-        if min_length < 1:
-            raise ValueError('\'min_length\' too small.')
-        valid = np.ones((self.tracks.shape[0])).astype(bool)
-        good_len = np.sum(self.tracks[:, 2:] != -1, axis=1) >= min_length
-        # Remove tracks which do not have an observation in most recent frame.
-        not_headless = (self.tracks[:, -1] != -1)
-        keepers = np.logical_and.reduce((valid, good_len, not_headless))
-        returned_tracks = self.tracks[keepers, :].copy()
-        return returned_tracks
-
-    def draw_tracks(self, out, tracks):
-        """ Visualize tracks all overlayed on a single image.
-    Inputs
-      out - numpy uint8 image sized HxWx3 upon which tracks are overlayed.
-      tracks - M x (2+L) sized matrix storing track info.
-    """
-        # Store the number of points per camera.
-        pts_mem = self.all_pts
-        N = len(pts_mem)  # Number of cameras/images.
-        # Get offset ids needed to reference into pts_mem.
-        offsets = self.get_offsets()
-        # Width of track and point circles to be drawn.
-        stroke = 1
-        # Iterate through each track and draw it.
-        for track in tracks:
-            clr = myjet[int(np.clip(np.floor(track[1] * 10), 0, 9)), :] * 255
-            for i in range(N - 1):
-                if track[i + 2] == -1 or track[i + 3] == -1:
-                    continue
-                offset1 = offsets[i]
-                offset2 = offsets[i + 1]
-                idx1 = int(track[i + 2] - offset1)
-                idx2 = int(track[i + 3] - offset2)
-                pt1 = pts_mem[i][:2, idx1]
-                pt2 = pts_mem[i + 1][:2, idx2]
-                p1 = (int(round(pt1[0])), int(round(pt1[1])))
-                p2 = (int(round(pt2[0])), int(round(pt2[1])))
-                cv2.line(out, p1, p2, clr, thickness=stroke, lineType=16)
-                # Draw end points of each track.
-                if i == N - 2:
-                    clr2 = (255, 0, 0)
-                    cv2.circle(out, p2, stroke, clr2, -1, lineType=16)
-
-
-class VideoStreamer(object):
-    """ Class to help process image streams. Three types of possible inputs:"
-    1.) USB Webcam.
-    2.) A directory of images (files in directory matching 'img_glob').
-    3.) A video file, such as an .mp4 or .avi file.
-  """
-    def __init__(self, basedir, camid, height, width, skip, img_glob):
-        self.cap = []
-        self.camera = False
-        self.video_file = False
-        self.listing = []
-        self.sizer = [height, width]
-        self.i = 0
-        self.skip = skip
-        self.maxlen = 1000000
-        # If the "basedir" string is the word camera, then use a webcam.
-        if basedir == "camera/" or basedir == "camera":
-            print('==> Processing Webcam Input.')
-            self.cap = cv2.VideoCapture(camid)
-            self.listing = range(0, self.maxlen)
-            self.camera = True
-        else:
-            # Try to open as a video.
-            self.cap = cv2.VideoCapture(basedir)
-            lastbit = basedir[-4:len(basedir)]
-            if (type(self.cap) == list
-                    or not self.cap.isOpened()) and (lastbit == '.mp4'):
-                raise IOError('Cannot open movie file')
-            elif type(self.cap) != list and self.cap.isOpened() and (lastbit !=
-                                                                     '.txt'):
-                print('==> Processing Video Input.')
-                num_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-                self.listing = range(0, num_frames)
-                self.listing = self.listing[::self.skip]
-                self.camera = True
-                self.video_file = True
-                self.maxlen = len(self.listing)
-            else:
-                print('==> Processing Image Directory Input.')
-                search = os.path.join(basedir, img_glob)
-                self.listing = glob.glob(search)
-                self.listing.sort()
-                self.listing = self.listing[::self.skip]
-                self.maxlen = len(self.listing)
-                if self.maxlen == 0:
-                    raise IOError(
-                        'No images were found (maybe bad \'--img_glob\' parameter?)'
-                    )
-
-    def read_image(self, impath, img_size):
-        """ Read image as grayscale and resize to img_size.
-    Inputs
-      impath: Path to input image.
-      img_size: (W, H) tuple specifying resize size.
-    Returns
-      grayim: float32 numpy array sized H x W with values in range [0, 1].
-    """
-        grayim = cv2.imread(impath)
-        if grayim is None:
-            raise Exception('Error reading image %s' % impath)
-        # Image is resized via opencv.
-        interp = cv2.INTER_AREA
-        grayim = cv2.resize(grayim, (img_size[1], img_size[0]),
-                            interpolation=interp)
-        grayim = (grayim.astype('float32') / 255.)
-        return grayim
-
-    def next_frame(self):
-        """ Return the next frame, and increment internal counter.
-    Returns
-       image: Next H x W image.
-       status: True or False depending whether image was loaded.
-    """
-        if self.i == self.maxlen:
-            return (None, None, False)
-        if self.camera:
-            ret, input_image = self.cap.read()
-            if ret is False:
-                print(
-                    'VideoStreamer: Cannot get image from camera (maybe bad --camid?)'
-                )
-                return (None, None, False)
-            if self.video_file:
-                self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.listing[self.i])
-            input_image = cv2.resize(input_image,
-                                     (self.sizer[1], self.sizer[0]),
-                                     interpolation=cv2.INTER_AREA)
-            input_image = input_image.astype('float') / 255.0
-        else:
-            image_file = self.listing[self.i]
-            input_image = self.read_image(image_file, self.sizer)
-        # Increment internal counter.
-        self.i = self.i + 1
-        input_image = input_image.astype('float32')
-
-        gray_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
-        return (input_image, gray_image, True)
